69cf658a2c1542c070b4db5a15181415ba5d815e
Kuzicki <kuzmickii1@yandex.by>
2025-12-03 01:19:43 +0300

    Add clang_format and comment out flake8

--- lua/kuzicki/none-ls.lua
+++ lua/kuzicki/none-ls.lua
@@ -18,12 +18,13 @@ function M.config()
       formatting.stylua,
       formatting.prettier,
       formatting.black,
+      formatting.clang_format,
       formatting.djlint.with({
               args = { "--reformat", "-" }, -- Use reformatting via stdin
               filetypes = { "html", "jinja2" }, -- Add more if needed
           }),
       -- diagnostics.ruff,
-      require("none-ls.diagnostics.flake8").with({extra_args = {"--max-line-length", "88", "--ignore", "F405,F403,E203,W503"}}),
+      -- require("none-ls.diagnostics.flake8").with({extra_args = {"--max-line-length", "88", "--ignore", "F405,F403,E203,W503"}}),
       -- formatting.prettier.with {
       --   extra_filetypes = { "toml" },
       --   -- extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
